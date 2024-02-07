4c55f1e1cc9c748c941c734fa0095f703f99a7b3
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-07 16:11:00 +0530

    Add project files.

---
+++ Feb7Assessment/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Feb7Assessment.Models;
+
+namespace Feb7Assessment.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Feb7Assessment.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Feb7Assessment/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Feb7Assessment.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string MName { get; set; }
+        public string StatCast { get; set; }
+        public string Director { get; set; }
+        public string Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
